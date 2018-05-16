e617d8fc1683bf53c68425e3af0cfb4755d93a22
floathandthing <springtrap.bite87@gmail.com>
2018-05-16 19:33:29 +0100

    First version, it works!

--- PortScanner/Program.cs
+++ PortScanner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Sockets;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,43 @@ namespace PortScanner
     {
         static void Main(string[] args)
         {
-            /*list of features:
-             * ability to portscan
-             * command line parameters
-             * port allocation recogniser (label common port numbers)
-            */
+            //program variable initialisation
+            string address;
+            int port = 1;
+
+            if (args.Length != 0)
+            { address = args[0]; } //get address to target from command line
+            else
+            {
+                //if none is specified
+                Console.Write("Usage: " + AppDomain.CurrentDomain.FriendlyName + " <address>\nEnter address to target: ");
+                address = Console.ReadLine();
+            }
+
+            while (port <= 65535)
+            {
+                try
+                {
+                    TcpClient tcp = new TcpClient();
+                    //connect to the server 
+                    Console.WriteLine("Trying port " + port);
+                    tcp.Connect(address, port); //if the connection is unsuccessful, the error is triggered.
+
+                    //otherwise, report an open port.
+                    Console.WriteLine("Port " + port + " is open!");
+                    tcp.Close();
+                }
+                catch (Exception err)
+                {
+                    if (!err.Message.Contains("actively refused"))
+                    {
+                        Console.WriteLine("Error: " + err.Message);
+                        break;
+                    }
+                }
+                port++;
+            }
+
         }
     }
 }
