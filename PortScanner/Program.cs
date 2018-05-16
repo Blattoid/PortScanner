e4cdf0e6876b9a869f4ccfa686ecb15aefe08989
floathandthing <springtrap.bite87@gmail.com>
2018-05-16 20:51:15 +0100

    Adds port range capability

--- PortScanner/Program.cs
+++ PortScanner/Program.cs
@@ -1,28 +1,79 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Net.Sockets;
 
 namespace PortScanner
 {
     class Program
     {
+        public static void parsePortRange(string input)
+        {                 
+            //get port range
+            try
+            {
+                port = Convert.ToInt16(Regex.Split(input, ":")[0]);
+                maxport = Convert.ToInt16(Regex.Split(input, ":")[1]);
+
+                //idiotproofing
+                if (port > maxport) { throw new ArgumentException("Start port is bigger than end port."); }
+                if (port < 1) { throw new ArgumentException("Start port is smaller than 1."); }
+                if (port > 65536) { throw new ArgumentException("Start port is bigger than 65535."); }
+                if (maxport > 65536) { throw new ArgumentException("End port is bigger than 65535."); }
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+        }
+
+        public static int port = 1;
+        public static int maxport = 65535;
         static void Main(string[] args)
         {
             //program variable initialisation
-            string address;
-            int port = 1;
+            string address = "";
             bool interactiveMode = false;
 
-            if (args.Length != 0)
+            if (args.Length > 0)
             { address = args[0]; } //get address to target from command line
-            else
+            if (args.Length > 1)
+            {
+                try
+                {
+                    parsePortRange(args[1]);
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine("Error parsing port range: " + err.Message);
+                    goto end;
+                }
+            }
+
+            if (args.Length == 0)
             {
                 interactiveMode = true; //since no parameters were passed, it it reasonable to assume that the program may not have been launched from the command prompt. Thus, interactive mode.
                 //if none is specified
-                Console.Write("Usage: " + AppDomain.CurrentDomain.FriendlyName + " <address>\nEnter address to target: ");
+                Console.Write("Usage: " + AppDomain.CurrentDomain.FriendlyName + " <address> [startport:endport]\nEnter address to target: ");
                 address = Console.ReadLine();
+
+                //port range
+                Console.Write("Enter port range (leave blank for none): ");
+                string range = Console.ReadLine();
+                if (range != "")
+                {
+                    try
+                    {
+                        parsePortRange(range);
+                    }
+                    catch (Exception err)
+                    {
+                        Console.WriteLine("Error parsing port range: " + err.Message);
+                        goto end;
+                    }
+                }
             }
 
-            while (port <= 65535)
+            while (port <= maxport)
             {
                 try
                 {
@@ -46,6 +97,7 @@ namespace PortScanner
                 port++;
             }
 
+            end: //label for teleportation to the end. kill the ender dragon.
             //if in interactive mode, wait for keypress before exit.
             if (interactiveMode)
             {
